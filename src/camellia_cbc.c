4c12202a8a473f3d59ed4fd4de73736ce5892c4e
Łukasz Hejnak (LeHack) <lehack-ghub@lehack.pl>
2017-05-27 01:44:51 +0200

    Camellia CBC: Added option to specify key length (128, 192 or 256), hardcoded IV vector (should be derived from key), fixed padding when input is an exact multiple of block size. Also hardcoded blocksize as per Camillia specification to 128B.

--- src/camellia_cbc.c
+++ src/camellia_cbc.c
@@ -3,10 +3,10 @@
  * Camellia in CBC mode with PKCS#7 padding
  *
  * Syntax:
- *  ./camellia_cbc [encrypt|decrypt] [input.file] [output.file]
+ *  ./camellia_cbc [encrypt|decrypt] [keylength] [input.file] [output.file]
  * Example:
- *  ./camellia_cbc encrypt myfile.doc myfile.doc.enc
- *  ./camellia_cbc decrypt myfile.doc.enc myfile.doc
+ *  ./camellia_cbc encrypt 256 myfile.doc myfile.doc.enc
+ *  ./camellia_cbc decrypt 256 myfile.doc.enc myfile.doc
  *
  * !!! WARNING !!!
  * This is a proof of concept. DO NOT use it to encode anything you actually want to protect.
@@ -24,53 +24,53 @@
 #include <string.h>
 #include <unistd.h>
 
-#define IP_SIZE 16
-#define OP_SIZE 16
+#define BS 16 // Camellia uses a 128B / 16b blocksize
 #define SECRET "klucz"
 
 CAMELLIA_KEY key;
-unsigned char iv[8];
+unsigned char iv[] = "abcdefgh";
 
-int generate_key() {
-    int fd;
-    if ((fd = open("/dev/random", O_RDONLY)) == -1)
-        perror("open error");
+int print_errors(const char *msg) {
+    unsigned long err;
+    int errFound = 0;
 
-    if ((read(fd, iv, 8)) == -1)
-        perror("read iv error");
+    while ((err = ERR_get_error())) {
+        errFound++;
+        printf("%s: %s\n", msg, ERR_error_string(err, NULL));
+    }
 
-    close(fd);
-    return 0;
+    return errFound;
 }
 
-int decrypt(int infd, int outfd) {
-    unsigned char inbuff[OP_SIZE], outbuf[IP_SIZE];
-    int n = 0, bufready = 0, prev_n = 0, olen = 0;
+int decrypt(int keylen, int infd, int outfd) {
+    unsigned char inbuff[BS], outbuf[BS]; // could be optimized by runtime allocation
+    int n = 0, outbufready = 0, prev_n = 0, padding = 0;
 
     while (1) {
-        if (n > 0) {
-            bzero(&outbuf, IP_SIZE);
-            Camellia_cbc_encrypt(inbuff, outbuf, 16, &key, iv, 0);
-            bufready = 1;
+        if (n) {
+            bzero(outbuf, BS);
+            Camellia_cbc_encrypt(inbuff, outbuf, BS, &key, iv, 0);
+            if (print_errors("Problem while decrypting data block"))
+                return 1;
+            outbufready = 1;
         }
 
-        bzero(&inbuff, OP_SIZE);
-        if ((n = read(infd, inbuff, OP_SIZE)) == -1) {
+        bzero(inbuff, BS);
+        if ((n = read(infd, inbuff, BS)) == -1) {
             perror("read error");
             break;
         }
 
-        if (bufready && n == 0) {
-            olen = IP_SIZE - outbuf[IP_SIZE - 1];
-            if (prev_n == OP_SIZE)
-                olen = IP_SIZE;
-            if ((write(outfd, outbuf, olen)) == -1)
+        if (!outbufready)
+            continue;
+
+        if (n && write(outfd, outbuf, BS) == -1)
+            perror("write error");
+        else if (!n) {
+            padding = outbuf[BS - 1];
+            if (BS - padding > 0 && write(outfd, outbuf, BS - padding) == -1)
                 perror("write error");
             break;
-        }
-        else if (bufready && n == OP_SIZE) {
-            if ((write(outfd, outbuf, n)) == -1)
-                perror("write error");
         }
 
         prev_n = n;
@@ -79,27 +79,30 @@ int decrypt(int infd, int outfd) {
     return 1;
 }
 
-int encrypt(int infd, int outfd) {
-    unsigned char inbuff[IP_SIZE], outbuf[OP_SIZE];
-    int n, i;
+int encrypt(int keylen, int infd, int outfd) {
+    unsigned char inbuf[BS], outbuf[BS]; // could be optimized by runtime allocation
+    int n = 0, i = 0;
 
     while (1) {
-        bzero(&inbuff, IP_SIZE);
-
-        if ((n = read(infd, inbuff, IP_SIZE)) == -1) {
+        bzero(inbuf, BS);
+        if ((n = read(infd, inbuf, BS)) == -1) {
             perror("read error");
             break;
         }
-        else if (n == 0)
-            break;
-        else if (n < IP_SIZE) {
-            for(i = n; i < IP_SIZE; i++)
-                inbuff[i] = IP_SIZE - n;
-        }
-        Camellia_cbc_encrypt(inbuff, outbuf, 16, &key, iv, 1);
+        // add padding, if required
+        for(i = n; i < BS; i++)
+            inbuf[i] = BS - n;
 
-        if ((n = write(outfd, outbuf, IP_SIZE)) == -1)
+        bzero(outbuf, BS);
+        Camellia_cbc_encrypt(inbuf, outbuf, BS, &key, iv, 1);
+        if (print_errors("Problem while encrypting data block"))
+            return 1;
+
+        if (write(outfd, outbuf, BS) == -1)
             perror("write error");
+
+        if (n < BS)
+            break; // exit
     }
 
     return 1;
@@ -116,37 +119,42 @@ int main (int argc, char *argv[]) {
             mode = 2;
 
         // reset mode if one of params is missing
-        if (argv[2] == NULL || argv[3] == NULL)
+        if (argv[2] == NULL || argv[3] == NULL || argv[4] == NULL)
             mode = 0;
     }
 
     if (mode == 0) {
         printf("Bad or incomplete parameters\n");
-        printf("Syntax: encrypt/decrypt infile outfile\n");
+        printf("Syntax: encrypt/decrypt keylength infile outfile\n");
         return 1;
     }
 
-    flags1 = flags1 | O_RDONLY;
-    flags2 = flags2 | O_RDONLY;
-    flags2 = flags2 | O_WRONLY;
-    flags2 = flags2 | O_CREAT;
+    flags1 = O_RDONLY;
+    flags2 = O_RDONLY | O_WRONLY | O_CREAT;
 
-    // wygenerowanie zestawu kluczy
-    Camellia_set_key(SECRET, 128, &key);
+    int keylen = 0;
+    sscanf(argv[2], "%d", &keylen);
+    if (keylen != 128 && keylen != 192 && keylen != 256) {
+        printf("Error: key length must be one of: 128, 192 or 256.\n");
+        return 1;
+    }
 
-    ERR_load_crypto_strings();
-
-    if ((infd = open(argv[2], flags1, S_IRUSR | S_IWUSR)) == -1)
+    if ((infd = open(argv[3], flags1, S_IRUSR | S_IWUSR)) == -1)
         perror("open input file error");
 
-    if ((outfd = open(argv[3], flags2, S_IRUSR | S_IWUSR)) == -1)
+    if ((outfd = open(argv[4], flags2, S_IRUSR | S_IWUSR)) == -1)
         perror("open output file error");
 
     ftruncate(outfd, 0);
+    ERR_load_crypto_strings();
+
+    // generate keys
+    Camellia_set_key(SECRET, keylen, &key);
+
     if (mode == 1)
-        encrypt(infd, outfd);
+        encrypt(keylen, infd, outfd);
     else
-        decrypt(infd, outfd);
+        decrypt(keylen, infd, outfd);
 
     close(infd);
     fsync(outfd);
